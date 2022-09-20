2829bd9df9fe6cffa339e0f74e192f14c094459f
slava-team <vyacheslav.legedin@bullhorn.com>
2022-09-20 18:43:59 +0300

    Direct Job updates

--- AdaptDebug/src/main/java/pages/W_DirectJob_NewJobOrder.java
+++ AdaptDebug/src/main/java/pages/W_DirectJob_NewJobOrder.java
@@ -1,4 +1,36 @@
 package pages;
 
-public class W_DirectJob_NewJobOrder {
+import _helpers.MySoftAssert;
+import _helpers.TestBase;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class W_DirectJob_NewJobOrder extends TestBase {
+
+    public String windowTitle  =   "New Direct Job Order";
+
+    private By ClientName = By.xpath("//*[@id=\"CLIENT$W_US_NewDirectJobOrder$W_US_NewDirectJobOrder\"]");
+
+    private By ContactName = By.xpath("//*[@id=\"REQUESTOR$W_US_NewDirectJobOrder$W_US_NewDirectJobOrder\"]");
+
+    private By JobTitle = By.xpath("//*[@id=\"JOBTITLE$W_US_NewDirectJobOrder$W_US_NewDirectJobOrder\"]");
+
+    public SelenideElement fJobTitle() {return $(JobTitle);}
+
+    public SelenideElement ClientNameField () {return $(ClientName);}
+
+    public SelenideElement ContactNameField() {return $(ContactName);}
+
+    public void verifyClientName (MySoftAssert softAssert, String title) {
+
+        softAssert.assertEquals(title,  ClientNameField().getText());
+    }
+
+    public void verifyContactName (MySoftAssert softAssert, String title) {
+
+        softAssert.assertEquals(title,  ContactNameField().getText());
+    }
+
 }
